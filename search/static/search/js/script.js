d5ac1434a21520aa62fa2ac2c825ea49c991e1ac
Naomie <naomie.colombo@gmail.com>
2021-05-16 16:41:14 +0200

    map display with stores ok

--- search/static/search/js/script.js
+++ search/static/search/js/script.js
@@ -1,10 +1,8 @@
 
   		//initialize the leaflet map
 
-  		
-
-		console.log("bonjour");		
-		var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+ /*
+		var mymap = L.map('mapid').setView([51.505, -0.09], 10);
 
 
 		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmFvbWllLWNvIiwiYSI6ImNrb2toNWxmcjE0bDMycXJtN3JyeWh5MTUifQ.qd_YdCTV7VRg3TkhvhsFVw', {
@@ -14,4 +12,4 @@
 		    tileSize: 512,
 		    zoomOffset: -1,
 		    accessToken: 'pk.eyJ1IjoibmFvbWllLWNvIiwiYSI6ImNrb2toNWxmcjE0bDMycXJtN3JyeWh5MTUifQ.qd_YdCTV7VRg3TkhvhsFVw'
-		}).addTo(mymap);
+		}).addTo(mymap);*/
