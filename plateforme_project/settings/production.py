532d7023aa92d08c5102a1e883de772905c3b1f0
Naomie <naomie.colombo@gmail.com>
2021-05-19 22:33:52 +0200

    Launching production

--- plateforme_project/settings/production.py
+++ plateforme_project/settings/production.py
@@ -36,7 +36,7 @@ MIDDLEWARE += [
 
 
 # Static files settings
-PROJECT_ROOT = os.path.abspath(__file__)
+PROJECT_ROOT = os.path.dirname()
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
