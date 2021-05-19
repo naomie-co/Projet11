00ec98f8499c23b3fdd96ce71f9efffd2117c91f
Naomie <naomie.colombo@gmail.com>
2021-05-19 23:27:24 +0200

    Launching production

--- plateforme_project/settings/production.py
+++ plateforme_project/settings/production.py
@@ -36,13 +36,13 @@ MIDDLEWARE += [
 
 
 # Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
